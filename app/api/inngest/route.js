56e9a5ad9e10e2d61d57befb3fba2d17fa09e788
bhupesh <bhupeshverma29bv@gmail.com>
2025-12-20 01:07:32 +0530

    done

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,11 +2,7 @@ import { inngest } from "@/src/inngest/client";
 import { codeAgentFunction } from "@/src/inngest/functions";
 import { serve } from "inngest/next";
 
-
-
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    codeAgentFunction
-  ],
+  functions: [codeAgentFunction],
 });
